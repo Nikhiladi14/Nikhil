50630fd8f92e1933a2446dc5d8156c8d5fba66eb
Nikhiladi14 <112459971+Nikhiladi14@users.noreply.github.com>
2022-09-29 13:42:18 +0530

    Add files via upload

---
+++ day3,7Q.py
@@ -0,0 +1,20 @@
+def triangle(n):
+    k=n-1
+    for i in range(0,n+1):
+        for j in range(0,k+1):
+            print(end=" ")
+        k=k-1
+        for j in range(i,0,-1):
+            print(j," ", end="")
+        print()
+n=float(input("Enter no of rows:"))
+if(n<=0 or int(n)!=n):
+    print("INVALID INPUT")
+else:
+    triangle(int(n))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Secu
